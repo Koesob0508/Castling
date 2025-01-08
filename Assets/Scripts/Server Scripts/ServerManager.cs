a8aa4b318373801871bfd96e139eabc680c7da21
Koesob <epi3706@khu.ac.kr>
2025-01-08 15:43:25 +0900

    ServerManager에서 OnReceived하는거 반영

--- Assets/Scripts/Server Scripts/ServerManager.cs
+++ Assets/Scripts/Server Scripts/ServerManager.cs
@@ -1,14 +1,39 @@
-﻿using Unity.Netcode;
+﻿using Castling.Shared;
+using Unity.Netcode;
 
 public class ServerManager
 {
-    //public void Init()
-    //{
-    //    NetworkManager.Singleton.CustomMessagingManager.RegisterNamedMessageHandler("FromClient", )
-    //}
+    public void Init()
+    {
+        NetworkManager.Singleton.CustomMessagingManager.RegisterNamedMessageHandler("FromClient", OnReceivedClientMessage);
+    }
 
-    //private void OnReceivedClientMessage(ulong clientID, FastBufferReader reader)
-    //{
+    private void OnReceivedClientMessage(ulong clientID, FastBufferReader reader)
+    {
+        reader.ReadValueSafe(out ushort commandType);
 
-    //}
+        switch (commandType)
+        {
+            case CommandType.TryMovePiece:
+                reader.ReadValueSafe(out string uid);
+                reader.ReadValueSafe(out int destinationX);
+                reader.ReadValueSafe(out int destinationY);
+                TryMovePiece(uid, destinationX, destinationY);
+                break;
+            case CommandType.StartGame:
+                // Server에서는 실행하지 않음
+                break;
+            case CommandType.EndGame:
+                // Server에서는 실행하지 않음
+                break;
+            case CommandType.MovePieceResult:
+                // Server에서는 실행하지 않음
+                break;
+        }
+    }
+
+    private void TryMovePiece(string pieceUID, int x, int y)
+    {
+
+    }
 }
