a2f50451019ccaa78d8b12177de8a49d920531ec
Koesob <epi3706@khu.ac.kr>
2025-01-09 20:35:37 +0900

    Fragment Delivery로 바꿔서 이제 overflow 이슈 없음

---
+++ Assets/Scripts/Server Scripts/SerializeTool.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+using UnityEngine;
+
+namespace Castling.Server
+{
+    public class SerializeTool
+    {
+        public static byte[] Serialize<T>(T obj) where T : class
+        {
+            try
+            {
+                string json = JsonSerialize(obj);
+                byte[] bytes = Encoding.UTF8.GetBytes(json);
+
+                return bytes;
+            }
+            catch(Exception e)
+            {
+                Debug.LogError($"Serialization erro: {e.Message}");
+
+                return new byte[0];
+            }
+        }
+
+        public static T Deserialize<T>(byte[] bytes) where T : class
+        {
+            try
+            {
+                string json = Encoding.UTF8.GetString(bytes);
+                T obj = JsonDeserialize<T>(json);
+
+                return obj;
+            }
+            catch(Exception e)
+            {
+                Debug.LogError($"Deserialization error: {e.Message}");
+
+                return null;
+            }
+        }
+
+        public static string JsonSerialize<T>(T obj) where T : class
+        {
+            string json = JsonConvert.SerializeObject(obj);
+
+            return json;
+        }
+
+        public static T JsonDeserialize<T>(string json)
+        {
+            try
+            {
+                T obj = JsonConvert.DeserializeObject<T>(json);
+
+                return obj;
+            }
+            catch(JsonException ex)
+            {
+                Debug.LogError($"JSON Deserialization failed: {ex.Message}");
+
+                return default(T);
+            }
+        }
+    }
+}
