03565d3e708e2502d7a9ab7635edd54a7f1102f5
Koesob <epi3706@khu.ac.kr>
2025-01-09 18:25:53 +0900

    Test 파일들 수정

--- Assets/Tests/EditTests/PieceTests.cs
+++ Assets/Tests/EditTests/PieceTests.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
-namespace Assets.Tests.EditTests
+namespace Castling.Tests
 {
-    class PieceTests
+    [TestFixture]
+    public class PieceTests
     {
+
     }
 }
